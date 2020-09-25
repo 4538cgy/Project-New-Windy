ef57e2e9bfd2f3be4ff9d0985694071c74f38e31
4538cgy <58514224+4538cgy@users.noreply.github.com>
2020-09-25 17:02:09 +0900

    commit Basic Activity

---
+++ app/src/main/java/com/uos/project_new_windy/LobbyActivity.kt
@@ -0,0 +1,11 @@
+package com.uos.project_new_windy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LobbyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lobby)
+    }
+}

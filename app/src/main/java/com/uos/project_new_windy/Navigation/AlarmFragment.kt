d5fa7d9a64b796e28ba7f3d035e2d32f80fe134a
4538cgy <58514224+4538cgy@users.noreply.github.com>
2020-09-25 17:54:56 +0900

    setting Graddle

---
+++ app/src/main/java/com/uos/project_new_windy/Navigation/AlarmFragment.kt
@@ -0,0 +1,2 @@
+package com.uos.project_new_windy.Navigation
+
--- app/src/main/java/com/uos/project_new_windy/Setting/SettingActivity.kt
+++ app/src/main/java/com/uos/project_new_windy/Setting/SettingActivity.kt
@@ -2,6 +2,7 @@ package com.uos.project_new_windy.Setting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.uos.project_new_windy.R
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

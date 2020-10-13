585d0d7fa3b5376e1b62cfbe40aea0c5b3a621de
4538cgy <58514224+4538cgy@users.noreply.github.com>
2020-10-13 18:18:25 +0900

    commit detail view

--- app/src/main/java/com/uos/project_new_windy/Navigation_Lobby/DetailViewFragment.kt
+++ app/src/main/java/com/uos/project_new_windy/Navigation_Lobby/DetailViewFragment.kt
@@ -1,13 +1,29 @@
 package com.uos.project_new_windy.Navigation_Lobby
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.uos.project_new_windy.Model.AlarmDTO
+import com.uos.project_new_windy.Model.ContentDTO
 import com.uos.project_new_windy.R
+import com.uos.project_new_windy.Util.FcmPush
+import kotlinx.android.synthetic.main.fragment_detail.view.*
+import kotlinx.android.synthetic.main.item_detail.view.*
 
 class DetailViewFragment : Fragment() {
+
+    var firestore : FirebaseFirestore ? = null
+    var uid : String ? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,7 +31,156 @@ class DetailViewFragment : Fragment() {
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
 
+        firestore = FirebaseFirestore.getInstance()
+        uid = FirebaseAuth.getInstance().currentUser?.uid
+
+        view.fragment_detail_recycler.adapter = DetailViewRecyclerViewAdapter()
+        view.fragment_detail_recycler.layoutManager = LinearLayoutManager(activity)
 
         return view
+    }
+
+    inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList: ArrayList<String> = arrayListOf()
+
+        init {
+            firestore?.collection("contents")?.orderBy("timestamp")
+                ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    contentDTOs.clear()
+                    contentUidList.clear()
+
+                    if (querySnapshot == null)
+                        return@addSnapshotListener
+
+                    for (snapshot in querySnapshot!!.documents) {
+                        var item = snapshot.toObject(ContentDTO::class.java)
+                        contentDTOs.add(item!!)
+                        contentUidList.add(snapshot.id)
+                    }
+
+                    notifyDataSetChanged()
+                }
+
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view =
+                LayoutInflater.from(parent.context).inflate(R.layout.item_detail, parent, false)
+
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+            var viewholder = (holder as CustomViewHolder).itemView
+
+
+            //userId
+            viewholder.item_detail_textview_user_name.text = contentDTOs!![position].userId
+
+            //Image
+            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl)
+                .into(viewholder.item_detail_circleImageview_profile)
+
+            //Explain content
+            viewholder.item_detail_textview_explain_content.text = contentDTOs!![position].explain
+
+            //likes
+            viewholder.item_detail_textview_like_count.text =
+                "Likes" + contentDTOs!![position].favoriteCount
+
+
+            //THIS CODE IS WHEN THE BUTTON IS CLICKED
+            viewholder.item_detail_imagebutton_like.setOnClickListener {
+                favoriteEvent(position)
+            }
+
+
+            //this code is when the page is loaded
+            if (contentDTOs!![position].favorites.containsKey(uid)) {
+                //this is like status
+                viewholder.item_detail_imagebutton_like.setImageResource(R.drawable.ic_account)
+
+            } else {
+                //this is unlike status
+                viewholder.item_detail_imagebutton_like.setImageResource(R.drawable.ic_favorite_border)
+            }
+
+            //tHIS CODE IS WHEN THE profile image is clicked
+            viewholder.item_detail_circleImageview_profile.setOnClickListener {
+
+                var fragment = UserFragment()
+                var bundle = Bundle()
+                bundle.putString("destinationUid", contentDTOs[position].uid)
+                bundle.putString("userId", contentDTOs[position].userId)
+                fragment.arguments = bundle
+                activity?.supportFragmentManager?.beginTransaction()
+                    ?.replace(R.id.main_content, fragment)?.commit()
+
+            }
+
+            viewholder.item_detail_imagebutton_comment.setOnClickListener { v ->
+                var intent = Intent(v.context, CommentActivity::class.java)
+                intent.putExtra("contentUid", contentUidList[position])
+                intent.putExtra("destinationUid", contentDTOs[position].uid)
+                startActivity(intent)
+            }
+
+        }
+
+
+        fun favoriteEvent(position: Int) {
+
+            var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction { transaction ->
+
+
+                var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
+
+                if (contentDTO!!.favorites.containsKey(uid)) {
+                    //When the button is clicked
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount - 1
+                    contentDTO?.favorites.remove(uid)
+
+
+                } else {
+                    //When the button is not clicked
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount + 1
+                    contentDTO?.favorites[uid!!] = true
+
+                    favoriteAlarm(contentDTOs[position].uid!!)
+                }
+                transaction.set(tsDoc, contentDTO)
+
+
+            }
+
+
+        }
+
+        fun favoriteAlarm(destinationUid: String) {
+            var alarmDTO = AlarmDTO()
+            alarmDTO.destinationUid = destinationUid
+            alarmDTO.userId = FirebaseAuth.getInstance().currentUser?.email
+            alarmDTO.uid = FirebaseAuth.getInstance().currentUser?.uid
+            alarmDTO.kind = 0
+            alarmDTO.timestamp = System.currentTimeMillis()
+            FirebaseFirestore.getInstance().collection("alarms").document().set(alarmDTO)
+
+            var message =
+                FirebaseAuth.getInstance()?.currentUser?.email + getString(R.string.alarm_favorite)
+            FcmPush.instance.sendMessage(destinationUid, "HowlInstgram", message)
+        }
+
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
     }
 }

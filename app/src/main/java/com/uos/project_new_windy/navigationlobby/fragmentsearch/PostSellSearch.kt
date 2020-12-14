b4860cda36fccecf307a597b446590d783309ab7
4538cgy <58514224+4538cgy@users.noreply.github.com>
2020-12-14 19:07:14 +0900

    commit 판매 게시판 검색

--- app/src/main/java/com/uos/project_new_windy/navigationlobby/fragmentsearch/PostSellSearch.kt
+++ app/src/main/java/com/uos/project_new_windy/navigationlobby/fragmentsearch/PostSellSearch.kt
@@ -1,17 +1,35 @@
 package com.uos.project_new_windy.navigationlobby.fragmentsearch
 
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.uos.project_new_windy.R
+import com.uos.project_new_windy.bottomsheet.BottomSheetDialogContentOption
 import com.uos.project_new_windy.databinding.FragmentPostSellSearchBinding
+import com.uos.project_new_windy.databinding.ItemPostSellSearchResultBinding
+import com.uos.project_new_windy.databinding.ItemRecyclerSellBinding
+import com.uos.project_new_windy.databinding.ItemSellSearchCategoryBinding
+import com.uos.project_new_windy.model.CategorySellPostDTO
 import com.uos.project_new_windy.model.contentdto.ContentSellDTO
+import com.uos.project_new_windy.navigationlobby.CommentActivity
+import com.uos.project_new_windy.navigationlobby.DetailActivityRecyclerViewAdapter.contentadapter.ContentSellRecyclerViewAdapter
+import com.uos.project_new_windy.navigationlobby.UserFragment
+import com.uos.project_new_windy.navigationlobby.detailviewactivity.DetailSellViewActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -70,21 +88,14 @@ class PostSellSearch : Fragment() {
 
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        binding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_post_sell_search, container, false)
-        binding.fragmentpostsellsearch
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_post_sell_search, container, false)
+
+
 
 
 
@@ -107,6 +118,8 @@ class PostSellSearch : Fragment() {
         contentData.forEach {
             System.out.println("카테고리를 포함한 데이터 $it" )
         }
+
+        binding.fragmentPostSellSearchRecycler.adapter?.notifyDataSetChanged()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -126,26 +139,89 @@ class PostSellSearch : Fragment() {
 
         }
 
+        binding.fragmentPostSellSearchRecycler.adapter = PostSellSearchRecyclerViewAdapter(binding.root.context)
+        //binding.fragmentPostSellSearchRecycler.layoutManager = LinearLayoutManager(binding.root.context,LinearLayoutManager.VERTICAL,false)
+        binding.fragmentPostSellSearchRecycler.layoutManager = LinearLayoutManager(activity)
+        binding.fragmentPostSellSearchRecycler.adapter?.notifyDataSetChanged()
 
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PostSellSearch.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PostSellSearch().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+    inner class PostSellSearchRecyclerViewAdapter(context : Context) : RecyclerView.Adapter<PostSellSearchRecyclerViewAdapter.PostSellSearchRecyclerViewAdapterViewHolder>(){
+
+        var contentSellDTO : ArrayList<ContentSellDTO> = arrayListOf()
+
+        var data  = listOf<ContentSellDTO>()
+        init {
+            
+            System.out.println("리사이클러뷰 초기화아아아아아아아아아아앜")
+
+            contentSellDTO.addAll(contentData)
+
+
+            data = contentSellDTO
+
+            data.forEach {
+                System.out.println("카테고리를 포함한 데이터 datadatadata $it" )
             }
+
+            contentSellDTO.forEach {
+                System.out.println("카테고리를 포함한 데이터 dadadadadadadada $it" )
+            }
+
+            notifyDataSetChanged()
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostSellSearchRecyclerViewAdapter.PostSellSearchRecyclerViewAdapterViewHolder {
+
+            val binding = ItemPostSellSearchResultBinding.inflate(LayoutInflater.from(context),parent,false)
+            return PostSellSearchRecyclerViewAdapterViewHolder(binding)
+        }
+
+
+        override fun getItemCount(): Int {
+            return contentSellDTO.size
+        }
+
+        override fun onBindViewHolder(holder: PostSellSearchRecyclerViewAdapterViewHolder, position: Int) {
+            holder.onBind(contentSellDTO[position])
+
+            //아이템 자체 클릭
+            holder.binding.itemPostSellSearchResultConstAll.setOnClickListener {
+                var intent = Intent(holder.itemView.context,DetailSellViewActivity::class.java)
+                intent.apply {
+                    putExtra("uid" , contentSellDTO[position].uid)
+                    putExtra("userId",contentSellDTO[position].userId)
+                    putExtra("postUid",contentUidList[position])
+                    putExtra("cost",contentSellDTO[position].cost)
+                    putExtra("category",contentSellDTO[position].category)
+                    putExtra("imageList",contentSellDTO[position].imageDownLoadUrlList)
+                    putExtra("contentTime",contentSellDTO[position].time)
+                    putExtra("productExplain",contentSellDTO[position].productExplain)
+                    putExtra("explain",contentSellDTO[position].explain)
+                    //putExtra("sellerAddress",contentSellDTO[position].sellerAddress)
+
+
+                }
+                context?.startActivity(intent)
+            }
+
+            //사진 추가
+            if (data[position].imageDownLoadUrlList?.isEmpty() == false) {
+                Glide.with(holder.itemView.context)
+                    .load(data[position].imageDownLoadUrlList?.get(0))
+                    .into(holder.binding.itemPostSellSearchResultImageviewPhoto)
+            }
+
+
+        }
+
+        inner class PostSellSearchRecyclerViewAdapterViewHolder(val binding: ItemPostSellSearchResultBinding) : RecyclerView.ViewHolder(binding.root){
+            fun onBind(data: ContentSellDTO){
+                binding.itempostsellsearchresult = data
+            }
+        }
+
+
     }
 }

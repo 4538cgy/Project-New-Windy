626825a643fcd9051c302390be6d99e1bc4ed8aa
4538cgy <58514224+4538cgy@users.noreply.github.com>
2020-12-17 00:29:24 +0900

    채팅 버그 수정

--- app/src/main/java/com/uos/project_new_windy/chat/ChatActivity.kt
+++ app/src/main/java/com/uos/project_new_windy/chat/ChatActivity.kt
@@ -13,6 +13,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.firestore.FirebaseFirestore
 import com.uos.project_new_windy.R
 import com.uos.project_new_windy.databinding.ActivityChatBinding
 import com.uos.project_new_windy.databinding.ItemChatBubbleBinding
@@ -24,18 +25,18 @@ import java.util.*
 
 class ChatActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityChatBinding
-    var uid : String ? = null
-    var destinationUid : String ? = null
-    var simpleDateFormat : SimpleDateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm")
-    var chatRoomUid : String ? = null
+    lateinit var binding: ActivityChatBinding
+    var uid: String? = null
+    var destinationUid: String? = null
+    var simpleDateFormat: SimpleDateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm")
+    var chatRoomUid: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_chat)
         binding.activitychat = this@ChatActivity
 
-        
+
         //채팅을 요구하는 아이디 [ 단말기에 로그인 된 ]
         uid = FirebaseAuth.getInstance().currentUser?.uid
         //채팅을 당하는 아이디
@@ -43,13 +44,13 @@ class ChatActivity : AppCompatActivity() {
 
         binding.activityChatImagebuttonUpload.setOnClickListener {
 
-            var chatDTOs  = ChatDTO()
+            var chatDTOs = ChatDTO()
 
 
             chatDTOs.users.put(uid!!, true);
             chatDTOs.users.put(destinationUid!!, true)
 
-            if (chatRoomUid == null){
+            if (chatRoomUid == null) {
                 FirebaseDatabase.getInstance().getReference().child("chatrooms").push().setValue(
                     chatDTOs).addOnSuccessListener {
 
@@ -57,15 +58,16 @@ class ChatActivity : AppCompatActivity() {
                     checkChatRoom()
                 }
 
-            }else{
+            } else {
                 var comment = ChatDTO.Comment()
                 comment.uid = uid;
                 comment.message = binding.activityChatEdittextExplain.text.toString()
                 comment.timestamp = ServerValue.TIMESTAMP
-                FirebaseDatabase.getInstance().getReference().child("chatrooms").child(chatRoomUid!!).child(
-                    "comments").push().setValue(comment).addOnCompleteListener {
-                    binding.activityChatEdittextExplain.setText(" ")
-                }
+                FirebaseDatabase.getInstance().getReference().child("chatrooms")
+                    .child(chatRoomUid!!).child(
+                        "comments").push().setValue(comment).addOnCompleteListener {
+                        binding.activityChatEdittextExplain.setText(" ")
+                    }
             }
 
         }
@@ -73,78 +75,88 @@ class ChatActivity : AppCompatActivity() {
         checkChatRoom()
     }
 
-    fun checkChatRoom(){
-        FirebaseDatabase.getInstance().getReference().child("chatrooms").orderByChild("users/" + uid).equalTo(
-            true).addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                var chatDTOs: ChatDTO = snapshot.getValue(ChatDTO::class.java)!!
+    fun checkChatRoom() {
 
-                if (chatDTOs.users.containsKey(destinationUid)) {
-                    chatRoomUid = snapshot.key
-                    binding.activityChatRecyclerBuble.layoutManager = LinearLayoutManager(
-                        applicationContext,
-                        LinearLayoutManager.VERTICAL,
-                        false)
-                    binding.activityChatRecyclerBuble.adapter = RecyclerViewAdapter()
+
+        FirebaseDatabase.getInstance().getReference().child("chatrooms")
+            .orderByChild("users/" + uid).equalTo(
+                true).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    snapshot.children.forEach {
+
+                        var chatDTOs: ChatDTO = it.getValue(ChatDTO::class.java)!!
+
+                        if (chatDTOs.users.containsKey(destinationUid)) {
+                            chatRoomUid = it.key
+                            binding.activityChatRecyclerBuble.layoutManager = LinearLayoutManager(
+                                applicationContext,
+                                LinearLayoutManager.VERTICAL,
+                                false)
+                            binding.activityChatRecyclerBuble.adapter = RecyclerViewAdapter()
+                        }
+                    }
                 }
-            }
 
-            override fun onCancelled(error: DatabaseError) {
-                System.out.println("checkChatRoom 데이터 조회 실패")
-            }
+                override fun onCancelled(error: DatabaseError) {
+                    System.out.println("checkChatRoom 데이터 조회 실패")
+                }
 
 
-        })
+            })
+
+
     }
 
-    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.MessageViewHolder>(){
+    inner class RecyclerViewAdapter :
+        RecyclerView.Adapter<RecyclerViewAdapter.MessageViewHolder>() {
 
-        var comments : ArrayList<ChatDTO.Comment> = arrayListOf()
-        var userModel  = UserModel()
-
+        var comments: ArrayList<ChatDTO.Comment> = arrayListOf()
+        var userModel = UserModel()
 
 
         init {
 
-            FirebaseDatabase.getInstance().getReference().child("users").child(destinationUid!!).addListenerForSingleValueEvent(
-                object : ValueEventListener {
+            FirebaseDatabase.getInstance().getReference().child("users").child(destinationUid!!)
+                .addListenerForSingleValueEvent(
+                    object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            userModel = snapshot.getValue(UserModel::class.java)!!
+                            getMessageList()
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            TODO("Not yet implemented")
+                        }
+
+
+                    })
+        }
+
+        fun getMessageList() {
+
+            FirebaseDatabase.getInstance().getReference().child("chatrooms").child(chatRoomUid!!)
+                .child(
+                    "comments").addValueEventListener(object : ValueEventListener {
                     override fun onDataChange(snapshot: DataSnapshot) {
-                        userModel = snapshot.getValue(UserModel::class.java)!!
-                        getMessageList()
+                        comments.clear()
+
+
+                        snapshot.children.forEach {
+                            comments.add(it.getValue(ChatDTO.Comment::class.java)!!)
+                        }
+
+                        notifyDataSetChanged()
+
+                        binding.activityChatRecyclerBuble.scrollToPosition(comments.size - 1)
+
                     }
 
                     override fun onCancelled(error: DatabaseError) {
                         TODO("Not yet implemented")
                     }
 
-
                 })
-        }
-
-        fun getMessageList(){
-
-            FirebaseDatabase.getInstance().getReference().child("chatrooms").child(chatRoomUid!!).child(
-                "comments").addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    comments.clear()
-
-
-                    snapshot.children.forEach {
-                        comments.add(it.getValue(ChatDTO.Comment::class.java)!!)
-                    }
-
-                    notifyDataSetChanged()
-
-                    binding.activityChatRecyclerBuble.scrollToPosition(comments.size - 1)
-
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-
-            })
-
 
 
         }
@@ -163,16 +175,15 @@ class ChatActivity : AppCompatActivity() {
             holder.onBind(comments[position])
 
             //내가 보낸 메세지
-            if (comments[position].uid.equals(uid))
-            {
+            if (comments[position].uid.equals(uid)) {
                 holder.binding.messageItemTextViewMessage.text = comments[position].message
                 holder.binding.messageItemTextViewMessage.setBackgroundResource(R.drawable.background_round_white_stroke_black)
                 holder.binding.messageItemLinearlayoutDestination.visibility = View.INVISIBLE
                 holder.binding.messageItemTextViewMessage.textSize = 25F
                 holder.binding.messageItemLinearlayoutMain.gravity = Gravity.RIGHT
 
-            //상대방이 보낸 메세지
-            }else{
+                //상대방이 보낸 메세지
+            } else {
                 /*
                 Glide.with(holder.itemView.context)
                     .load(userModel.profileImageUrl)
@@ -204,8 +215,8 @@ class ChatActivity : AppCompatActivity() {
         }
 
         inner class MessageViewHolder(var binding: ItemChatBubbleBinding) : RecyclerView.ViewHolder(
-            binding.root){
-            fun onBind(data: ChatDTO.Comment){
+            binding.root) {
+            fun onBind(data: ChatDTO.Comment) {
                 binding.itemchatbubble = data
             }
 
@@ -217,6 +228,6 @@ class ChatActivity : AppCompatActivity() {
     override fun onBackPressed() {
         //super.onBackPressed()
         finish()
-        overridePendingTransition(R.anim.fromleft,R.anim.fromright)
+        overridePendingTransition(R.anim.fromleft, R.anim.fromright)
     }
 }

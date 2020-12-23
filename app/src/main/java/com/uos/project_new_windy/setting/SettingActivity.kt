de7af799fa4f757c912a6c349c7d9d52d27bfbf8
4538cgy <58514224+4538cgy@users.noreply.github.com>
2020-12-24 00:36:39 +0900

    setting 임시 저장

--- app/src/main/java/com/uos/project_new_windy/setting/SettingActivity.kt
+++ app/src/main/java/com/uos/project_new_windy/setting/SettingActivity.kt
@@ -2,6 +2,7 @@ package com.uos.project_new_windy.setting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -16,10 +17,11 @@ class SettingActivity : AppCompatActivity() {
     lateinit var binding: ActivitySettingBinding
     val firestore = FirebaseFirestore.getInstance()
     val auth = FirebaseAuth.getInstance()
+    var userModel = UserModel()
 
     init {
 
-        var userModel = UserModel()
+
 
         firestore.collection("userInfo").document("userData").collection(auth.currentUser?.uid!!).document("accountInfo").addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
 
@@ -29,7 +31,7 @@ class SettingActivity : AppCompatActivity() {
             }
         }
 
-        
+
     }
 
 
@@ -52,6 +54,40 @@ class SettingActivity : AppCompatActivity() {
                 }
 
             }
+
+        //로그인 정보의 이메일
+        binding.activitySettingTextviewEmail.text = auth.currentUser?.email.toString()
+
+        //계정 정보의 이메일
+        binding.activitySettingTextviewEmailaddress.text = auth.currentUser?.email.toString()
+
+        //계정 정보의 닉네임
+        binding.activitySettingTextviewUsernickname.text = userModel.address.toString()
+
+        //계정 정보의 주소
+        binding.activitySettingTextviewAddress.text = userModel.phoneNumber.toString()
+
+        //수정 버튼
+        binding.activitySettingButtonEditUserInfo.setOnClickListener {
+
+        }
+
+        //푸쉬 전체 알람 끄기
+        binding.activitySettingSwitchPush.setOnCheckedChangeListener { buttonView, isChecked ->
+            Toast.makeText(binding.root.context , "푸쉬 알람이 꺼졌습니다." , Toast.LENGTH_SHORT).show()
+        }
+        
+        //마케팅 광고 알람 끄기
+        binding.activitySettingSwitchMaketting.setOnCheckedChangeListener { buttonView, isChecked ->
+            Toast.makeText(binding.root.context , "광고 알람이 꺼졌습니다." , Toast.LENGTH_SHORT).show()
+
+        }
+
+        //공지 알람 끄기
+        binding.activitySettingSwitchNotice.setOnCheckedChangeListener { buttonView, isChecked ->
+            Toast.makeText(binding.root.context , "공지 알람이 꺼졌습니다." , Toast.LENGTH_SHORT).show()
+            
+        }
 
     }
 }
---
+++ app/src/main/java/com/uos/project_new_windy/setting/SettingUserInfoEditActivity.kt
@@ -0,0 +1,12 @@
+package com.uos.project_new_windy.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uos.project_new_windy.R
+
+class SettingUserInfoEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_user_info_edit)
+    }
+}

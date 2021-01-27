b2f63dd0a89b2b01afac5d76c439a6eb90d80aee
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-01-27 18:03:02 +0900

    commit 관리자 추가

--- app/src/main/java/com/uos/project_new_windy/bottomsheet/BottomSheetDialogContentOption.kt
+++ app/src/main/java/com/uos/project_new_windy/bottomsheet/BottomSheetDialogContentOption.kt
@@ -58,7 +58,8 @@ class BottomSheetDialogContentOption : BottomSheetDialogFragment(){
         if(FirebaseAuth.getInstance().currentUser?.uid.toString().equals("6TzkruCHS2YufEURE9vtK4VdFu52")
             || FirebaseAuth.getInstance().currentUser?.uid.toString().equals("jOMoLi0YgZUJUDypzaXSVQ84cEU2")
             || FirebaseAuth.getInstance().currentUser?.email.toString().equals("hG9W4uIR4dOmLweh2XOHMs9HbBE3")
-            || FirebaseAuth.getInstance().currentUser?.email.toString().equals("ay72HtBWTWetM9JYE2VKYlmbYqh2"))
+            || FirebaseAuth.getInstance().currentUser?.email.toString().equals("ay72HtBWTWetM9JYE2VKYlmbYqh2")
+            || FirebaseAuth.getInstance().currentUser?.uid.toString().equals("dZvFUbfbL9NZ5SYygiFsmSrAmM63"))
         {
             println("삭제버튼이 보입니다.")
             binding.bottomSheetSelectContentOptionConstDelete.visibility = View.VISIBLE

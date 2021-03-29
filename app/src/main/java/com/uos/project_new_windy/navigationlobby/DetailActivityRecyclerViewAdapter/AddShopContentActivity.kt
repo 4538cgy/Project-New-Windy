dc380888383efc4c3cea86aae8d7768d00d1bbeb
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-03-29 12:11:16 +0900

    쇼핑게시판 베이스 추가

---
+++ app/src/main/java/com/uos/project_new_windy/navigationlobby/DetailActivityRecyclerViewAdapter/AddShopContentActivity.kt
@@ -0,0 +1,11 @@
+package com.uos.project_new_windy.navigationlobby.DetailActivityRecyclerViewAdapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddShopContentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_shop_content)
+    }
+}
---
+++ app/src/main/java/com/uos/project_new_windy/navigationlobby/DetailActivityRecyclerViewAdapter/addcontentadapter/AddShopContentActivityRecyclerViewAdapter.kt
@@ -0,0 +1,49 @@
+package com.uos.project_new_windy.navigationlobby.DetailActivityRecyclerViewAdapter.addcontentadapter
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.uos.project_new_windy.R
+import com.uos.project_new_windy.navigationlobby.AddSellContentActivity
+import kotlinx.android.synthetic.main.item_image_list.view.*
+
+class AddShopContentActivityRecyclerViewAdapter (var activity: AddSellContentActivity, var imageUriList : ArrayList<Uri>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.item_image_list,parent,false)
+        return CustomViewHolder(view)
+    }
+
+    inner class CustomViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        var viewHolder = (holder as CustomViewHolder).itemView
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+
+            Glide.with(holder.itemView.context)
+                .load(imageUriList[position])
+                .into(viewHolder.item_image_list_imageview)
+
+        }
+
+        viewHolder.item_image_list_const_all.setOnClickListener {
+            imageUriList.removeAt(position)
+
+            notifyDataSetChanged()
+        }
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return imageUriList.size
+    }
+}

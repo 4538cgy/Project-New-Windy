14b0054c208548708edefe47665d0415a09319d7
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-05-18 17:51:40 +0900

    data model

--- app/src/main/java/com/uos/project_new_windy/model/mallmodel/MallMainModel.kt
+++ app/src/main/java/com/uos/project_new_windy/model/mallmodel/MallMainModel.kt
@@ -1,7 +1,29 @@
 package com.uos.project_new_windy.model.mallmodel
 
 data class MallMainModel(
-    var title : String ? =null,
-    var cost : Long ? = null,
-    var salesCost : Long ? = null
-)
+    var title : String ? =null
+){
+    data class Product(
+        var title : String ? = null,
+        var cost : Long ? = null,
+        var salesCost : Long ? = null,
+        var imageUrlList : ArrayList<String> ? = null,
+        var thumbnailUrl : String ? = null,
+        var review : MutableMap<String,Review> = HashMap(),
+        var qna : MutableMap<String,Qna> = HashMap()
+    ){
+        data class Review(
+            var imageUrlList: ArrayList<String>? = null,
+            var comment : String ? = null,
+            var timestamp : Long ? = null,
+            var rating : Long? = null,
+            var commentUid : String ? = null
+        )
+        data class Qna(
+            var comment : String ? = null,
+            var timestamp: Long? = null,
+            var commentUid: String? = null
+        )
+    }
+
+}

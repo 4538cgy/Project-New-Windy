1f88e740bf50aec2a6d5b91fc685b2f06a98746e
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-05-27 16:04:55 +0900

    commit product upload base

--- app/src/main/java/com/uos/project_new_windy/newwindymall/AddProductActivity.kt
+++ app/src/main/java/com/uos/project_new_windy/newwindymall/AddProductActivity.kt
@@ -2,11 +2,19 @@ package com.uos.project_new_windy.newwindymall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.uos.project_new_windy.R
+import com.uos.project_new_windy.databinding.ActivityAddProductBinding
 
 class AddProductActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityAddProductBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_product)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_add_product)
+        binding.activityaddproduct = this
+
+        binding.activityAddProductImagebuttonClose.setOnClickListener { finish() }
     }
 }
--- app/src/main/java/com/uos/project_new_windy/newwindymall/NewWindyMain.kt
+++ app/src/main/java/com/uos/project_new_windy/newwindymall/NewWindyMain.kt
@@ -1,12 +1,16 @@
 package com.uos.project_new_windy.newwindymall
 
+import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import com.uos.project_new_windy.R
 import com.uos.project_new_windy.bottomsheet.BottomSheetDialogWriteCategory
 import com.uos.project_new_windy.bottomsheet.malloption.BottomSheetDialogMallOption
@@ -17,19 +21,38 @@ import com.uos.project_new_windy.model.mallmodel.MallMainModel
 class NewWindyMain : AppCompatActivity(), BottomSheetDialogMallOption.BottomSheetButtonClickListener {
 
     lateinit var binding : ActivityNewWindyMainBinding
-
+    private var isOpenFAB = false
     private var recyclerList = arrayListOf<MallMainModel.Product>()
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_new_windy_main)
         binding.activitynewwindymain = this
 
+        if(FirebaseAuth.getInstance().currentUser?.uid.toString().equals("6TzkruCHS2YufEURE9vtK4VdFu52")
+            || FirebaseAuth.getInstance().currentUser?.uid.toString().equals("jOMoLi0YgZUJUDypzaXSVQ84cEU2")
+            || FirebaseAuth.getInstance().currentUser?.email.toString().equals("hG9W4uIR4dOmLweh2XOHMs9HbBE3")
+            || FirebaseAuth.getInstance().currentUser?.email.toString().equals("ay72HtBWTWetM9JYE2VKYlmbYqh2")
+            || FirebaseAuth.getInstance().currentUser?.uid.toString().equals("dZvFUbfbL9NZ5SYygiFsmSrAmM63"))
+        {
+            binding.activityNewWindyMainButtonAddProduct.visibility = View.VISIBLE
+        }else {
+            binding.activityNewWindyMainButtonAddProduct.visibility = View.GONE
+
+        }
         binding.activityNewWindyMainImagebuttonClose.setOnClickListener { finish() }
         binding.activityNewWindyMainImagebuttonOption.setOnClickListener {
             val bottomSheetDialog : BottomSheetDialogMallOption = BottomSheetDialogMallOption()
 
             bottomSheetDialog.show(supportFragmentManager,"lol")
+        }
+
+        binding.activityNewWindyMainButtonAddProduct.setOnClickListener {
+            var intent = Intent(binding.root.context,AddProductActivity::class.java)
+            intent.apply {
+                startActivity(intent)
+            }
         }
 
         binding.activityNewWindyMainRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener(){
@@ -49,6 +72,27 @@ class NewWindyMain : AppCompatActivity(), BottomSheetDialogMallOption.BottomShee
     fun initRecyclerView(){
         binding.activityNewWindyMainRecycler.adapter = MallMainRecyclerViewAdapter()
         binding.activityNewWindyMainRecycler.layoutManager = LinearLayoutManager(binding.root.context,LinearLayoutManager.VERTICAL,false)
+    }
+
+    fun clickFab(view : View){
+        isOpenFAB = if (!isOpenFAB) {
+            ObjectAnimator.ofFloat(binding.activityNewWindyMainFabCart, "translationY", -200f)
+                .apply { start() }
+            ObjectAnimator.ofFloat(binding.activityNewWindyMainTextviewCart, "translationY", -200f)
+                .apply { start() }
+
+            binding.activityNewWindyMainTextviewCart.visibility = View.VISIBLE
+            true
+        } else {
+            ObjectAnimator.ofFloat(binding.activityNewWindyMainTextviewCart, "translationY", -0f)
+                .apply { start() }
+
+            ObjectAnimator.ofFloat(binding.activityNewWindyMainFabCart, "translationY", -0f)
+                .apply { start() }
+
+            binding.activityNewWindyMainTextviewCart.visibility = View.INVISIBLE
+            false
+        }
     }
 
     override fun onBottomSheetButtonClick(text: String) {

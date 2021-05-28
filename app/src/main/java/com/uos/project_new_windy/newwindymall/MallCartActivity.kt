6120cb6a4916b4b8d5e4fdef2ece2c71925a23cc
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-05-28 23:25:25 +0900

    commit cart

--- app/src/main/java/com/uos/project_new_windy/newwindymall/MallCartActivity.kt
+++ app/src/main/java/com/uos/project_new_windy/newwindymall/MallCartActivity.kt
@@ -2,13 +2,29 @@ package com.uos.project_new_windy.newwindymall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.uos.project_new_windy.R
 import com.uos.project_new_windy.databinding.ActivityMallCartBinding
+import com.uos.project_new_windy.databinding.ItemMallCartBinding
+import com.uos.project_new_windy.databinding.ItemNewWindyMallMainBinding
+import com.uos.project_new_windy.model.mallmodel.MallMainModel
+import java.text.DecimalFormat
 
 class MallCartActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMallCartBinding
+
+    private var recyclerDataList = arrayListOf<MallMainModel.Product>()
+    private var recyclerIdList = arrayListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_mall_cart)
@@ -16,5 +32,73 @@ class MallCartActivity : AppCompatActivity() {
 
         binding.activityMallCartImagebuttonClose.setOnClickListener { finish() }
         binding.activityMallCartButtonOrder.setOnClickListener {  }
+        binding.activityMallCartRecyclerview.adapter = CartRecyclerViewAdapter()
+        binding.activityMallCartRecyclerview.layoutManager = LinearLayoutManager(binding.root.context,LinearLayoutManager.VERTICAL,false)
+
+        //장바구니 데이터 가져오기
+        getCartData()
+    }
+
+    fun getCartData(){
+        FirebaseFirestore.getInstance().collection("userInfo").document("userData").collection(FirebaseAuth.getInstance().currentUser!!.uid).document("cart")
+            .addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+                if (documentSnapshot != null){
+                    if (documentSnapshot.exists()){
+                        var cartDTO = documentSnapshot.toObject(MallMainModel.CartDTO::class.java)
+                        getProductData(cartDTO!!)
+                    }
+                }
+            }
+    }
+
+    fun getProductData(data : MallMainModel.CartDTO){
+        var count = 0
+        data.productId.keys.forEach {
+            FirebaseFirestore.getInstance().collection("Mall").document("product").collection("product").document(it.toString())
+                .get().addOnSuccessListener {
+                    recyclerDataList.add(it.toObject(MallMainModel.Product::class.java)!!)
+                    recyclerIdList.add(it.id)
+                    count ++
+                    if (count == data.productId.keys.size){
+                        binding.activityMallCartRecyclerview.adapter!!.notifyDataSetChanged()
+                    }
+                }
+        }
+
+    }
+
+    inner class CartRecyclerViewAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val binding = ItemMallCartBinding.inflate(LayoutInflater.from(binding.root.context),parent,false)
+            return CartRecyclerViewHolder(binding)
+        }
+
+        override fun getItemCount(): Int {
+            if (recyclerDataList != null){
+                return recyclerDataList.size
+            }else{
+                return 0
+            }
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            (holder as CartRecyclerViewHolder).onBind(recyclerDataList[position])
+
+            holder.binding.itemMallCartTextviewTitle.text = recyclerDataList[position].title.toString()
+            holder.binding.itemMallCartTextviewCost.text = recyclerDataList[position].cost.toString()
+
+            Glide.with(holder.binding.root.context)
+                .load(recyclerDataList[position].imageUrlList!![0])
+                .centerInside()
+                .thumbnail(0.01f)
+                .into(holder.binding.itemMallCartImageviewProductImage)
+
+        }
+
+    }
+    class CartRecyclerViewHolder(val binding : ItemMallCartBinding) : RecyclerView.ViewHolder(binding.root){
+        fun onBind(data : MallMainModel.Product){
+            binding.itemmallcart = data
+        }
     }
 }
